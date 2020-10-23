9ff708e05e7909043fd6ae1d3c7b59bde228b0b9
StevenTedYT <45540516+StevenTedYT@users.noreply.github.com>
2020-10-23 23:45:20 +0200

    Avatar avec système de cherche ++

---
+++ Commande Avatar.js
@@ -0,0 +1,26 @@
+// By StevenTed
+
+const {MessageEmbed} = require('discord.js');
+
+module.exports.run = async(client, message, args) => {
+  const search = args[0] ? args[0].toLowerCase() : message.author.id;
+  const userID = message.guild.members.cache.get(search);
+  const userUsername = message.guild.members.cache.filter(e => e.user.username.toLowerCase().includes(search)).first();
+  const userNickname = message.guild.members.cache.filter(e => e.displayName.toLowerCase().includes(search)).first();
+  const userDiscrim = message.guild.members.cache.filter(e => e.user.discriminator.toLowerCase().includes(search)).first();
+  const userTag = message.guild.members.cache.filter(e => e.user.tag.toLowerCase().includes(search)).first();
+  const userMention = message.guild.members.cache.get(search.replace(/\D/g, ""));
+  const getUser = userID || userUsername || userNickname || userDiscrim || userTag || userMention;
+  const embed = new MessageEmbed()
+    .setColor('RANTOM')
+    .setTitle(`Avatar de ${getUser.user.tag} :`)
+    .setImage(getUser.user.displayAvatarURL({size: 2048, dynamic: true, fromat: "png"}))
+    .setTimestamp()
+    .setFooter(`Alpha v0.1.1`, `${message.author.displayAvatarURL({ dynamic: true, format: '', size: 2048 })}`)
+  return message.channel.send(embed).disableMentions
+};
+
+module.exports.help = {
+  name: "avatar",
+  aliases: ["pdp", "pfp", "pp", "Avatar", "AVATAR", "Pdp", "Pfp", "Pp"]
+};
