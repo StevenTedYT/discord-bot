b7adaba44d8f8f9339c2c65e40f363ed6fcc7575
StevenTedYT <45540516+StevenTedYT@users.noreply.github.com>
2020-10-31 00:05:06 +0100

    Update eval.js

--- eval.js
+++ eval.js
@@ -4,7 +4,7 @@ const User1 = 327878748466839552; // ton ID
 const User2 = 260831072831406090; // ID d'un pote a toi
 module.exports.run = async(client, message, args) => {
 	if(message.author.id == User1 || message.author.id == User2){ // Si tu met que une seul id tu peux supprime de || à User2
-		if(args.length == 0) return message.reply("le eval ne peux etre vide");
+		if(args.length == 0) return message.reply("Le eval ne peux etre vide");
 	    var evalCode = args.join(" ");
 	    let result = null;
 	    try {
