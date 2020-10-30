3e8f5b8c667a1811c75ab77bd27241db9baa8273
StevenTedYT <45540516+StevenTedYT@users.noreply.github.com>
2020-10-31 00:04:53 +0100

    Update eval.js

--- eval.js
+++ eval.js
@@ -1,9 +1,9 @@
 const Discord = require('discord.js');
 const util = require("util");
-const Steven = 327878748466839552;
-const Adel = 260831072831406090;
+const User1 = 327878748466839552; // ton ID
+const User2 = 260831072831406090; // ID d'un pote a toi
 module.exports.run = async(client, message, args) => {
-	if(message.author.id == Steven || message.author.id == Adel){
+	if(message.author.id == User1 || message.author.id == User2){ // Si tu met que une seul id tu peux supprime de || à User2
 		if(args.length == 0) return message.reply("le eval ne peux etre vide");
 	    var evalCode = args.join(" ");
 	    let result = null;
