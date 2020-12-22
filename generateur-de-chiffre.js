5906a36476f0adc4b7131a4791b89f4ad10fbd5f
Steven <45540516+StevenTedYT@users.noreply.github.com>
2020-12-22 22:28:16 +0100

    Update generateur-de-chiffre.js

--- generateur-de-chiffre.js
+++ generateur-de-chiffre.js
@@ -1,6 +1,6 @@
 const discord=require('discord.js');
 
-module.exports.run = async(client, message, args) => {
+module.exports.run=async(client,message,args)=>{
 function set(min,max)
 {
     return Math.floor(Math.random()*(max-min+1))+min;
