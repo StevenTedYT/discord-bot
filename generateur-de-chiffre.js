2ce6b23115e3b536652ea0106f5edde5715777c6
Steven <45540516+StevenTedYT@users.noreply.github.com>
2020-12-22 22:26:09 +0100

    Générateur de chiffre(Pour system d'argent par ex)

---
+++ generateur-de-chiffre.js
@@ -0,0 +1,15 @@
+const discord=require('discord.js');
+
+module.exports.run = async(client, message, args) => {
+function set(min,max)
+{
+    return Math.floor(Math.random()*(max-min+1))+min;
+}
+var random=set(0,20000)
+message.channel.send(random)
+}
+
+module.exports.help = {
+    name: "test",
+    aliases: ["t"]
+}
